b1d0a4cec3356c6e845cfe8d632361e3933314e2
github username <github email address>
2020-01-15 09:02:40 -0500

    Initial commit

---
+++ spec/breaking_bad_cli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BreakingBadCli do
+  it "has a version number" do
+    expect(BreakingBadCli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
